e41de8f5400d69dd9eb9e2a47d586709b256c082
Alex Kapelyukhovsky <akapelyukhovsky@ingenio.com>
2018-10-06 12:30:05 -0700

    update model like on dev env and fix cors limitation

--- server.js
+++ server.js
@@ -10,19 +10,18 @@ var port = process.env.PORT || 3008;
 var router = express.Router();
 
 router.get('/users/111/transactions/call-details/111', function(req, res) {
-    var resp = {
-      "calldetail": {
-        "id":111,
-        "listingId":222,
-        "advisorId":333,
-        "advisorName":"AdvisorName",
-        "consumerId":444,
-        "consumerName":"ConsumerName",
-        }
-    };
+    var resp = {"calldetail":{"id":111,"type":"call","listingId":222,"advisorId":333,"advisorName":"AdvisorName","consumerId":444,"consumerName":"ConsumerName","startDate":"7/18/2018 12:50 PM","endDate":"7/18/2018 1:02 PM","totalTime":12,"offerTime":1,"rate":5.99,"platformFee":4.4,"commission":50.48,"balanceUsed":10,"offerDescription":"1 Free minute of the offer \"Talk for Free Minutes from Mike Pace\"","feedbackRating":5,"feedbackComment":"Thank you Mike! I appreciate your honest, accurate readings!","bonus":true,"seamlessPayDetails":[{"payDate":"7/18/2018 1:02 PM","payType":"pay","payAmount":65.89,"cardType":"amex","cardNumberLast4Digits":"0545"},{"payDate":"7/18/2018 1:02 PM","payType":"pay","payAmount":65.89,"cardType":"amex","cardNumberLast4Digits":"0545"},{"payDate":"7/18/2018 1:02 PM","payType":"refund","payAmount":65.89,"cardType":"amex","cardNumberLast4Digits":"0545"}],"chargesDetails":[{"payDate":"7/18/2018 1:02 PM","payType":"pay","payAmount":65.89,"cardType":"master","cardNumberLast4Digits":"3032"}]}};
     res.json(resp);
 });
 
+var corsSettings = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-EpcApi-ID, X-Domain-ID");
+
+    next();
+};
+app.use(corsSettings);
 app.use('/', router);
 
 app.listen(port);
